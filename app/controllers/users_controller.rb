db6a4f942be28ddf3011ae1be740393981aa8841
Fernando Contreras <fercreek@gmail.com>
2017-08-23 17:28:18 -0500

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,15 +27,19 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    # respond_to do |format|
+    if @user.save
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+      # format.html { redirect_to @user, notice: 'User was successfully created.' }
+      # format.json { render :show, status: :created, location: @user }
+    else
+      render 'new'
+      # format.html { render :new }
+      # format.json { render json: @user.errors, status: :unprocessable_entity }
     end
+    # end
   end
 
   # PATCH/PUT /users/1
